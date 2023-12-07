d7df484f3a71807373fee70028217302e8e8780f
NIKATONI <102518913+NIKATONI@users.noreply.github.com>
2023-12-07 13:39:11 +0400

    AddedUrls

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import styles from './footer.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Footer() {
 
   const socials = [
     {
       src: "/1.png",
-      alt: "facebook"
+      alt: "facebook",
+      url: "https://www.facebook.com"
     },
     {
       src: "/2.png",
-      alt: "instagram"
+      alt: "instagram",
+      url: "https://www.instagram.com"
     },
     {
       src: "/3.png",
-      alt: "Picture of the author"
+      alt: "Picture of the author",
+      url: "https://www.twitter.com"
     },
     {
       src: "/4.png",
-      alt: "youtube"
+      alt: "youtube",
+      url: "https://www.youtube.com"
     }
   ]
 
@@ -30,7 +35,9 @@ export default function Footer() {
       <div className={styles.socialNewtworks}>
         {socials.map((item) => (
 
-          <Image className={styles.icon} src={item.src} width={25} height={25} alt={item.alt} />
+          <Link href={item.url} target="#">
+            <Image className={styles.icon} src={item.src} width={25} height={25} alt={item.alt} />
+          </Link>
 
         ))}
       </div>
